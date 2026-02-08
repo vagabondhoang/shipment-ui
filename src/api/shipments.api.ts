46706486c8c363190d23ac6f95f0aafbfdff4eaa
vagabondhoang <hoangtuananhnamdinh@gmail.com>
2026-02-08 21:42:55 +0700

    add env file

--- src/api/shipments.api.ts
+++ src/api/shipments.api.ts
@@ -1,6 +1,7 @@
 import type { Shipment, ShipmentStatus } from "@/types/shipment";
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL =  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+
 
 export interface FetchShipmentsParams {
   page: number;
